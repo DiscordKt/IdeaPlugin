623abc7fe789142b8d75513a0eb2884eab490c11
JakeJMattson <JakeJMattson@gmail.com>
2020-06-30 23:52:57 -0500

    Update KUtilsFramework.kt

--- src/main/kotlin/me/jakejmattson/kutils/framework/KUtilsFramework.kt
+++ src/main/kotlin/me/jakejmattson/kutils/framework/KUtilsFramework.kt
@@ -6,35 +6,27 @@ import com.intellij.ide.util.frameworkSupport.FrameworkSupportModel
 import com.intellij.openapi.module.*
 import com.intellij.openapi.roots.*
 import me.jakejmattson.kutils.utils.ICONS
-import javax.swing.*
+import javax.swing.JComponent
+
+private const val FRAMEWORK_ID = "me.jakejmattson.kutils.framework.KUtilsFramework"
 
 class KUtilsFramework : FrameworkTypeEx(FRAMEWORK_ID) {
-    override fun createProvider(): FrameworkSupportInModuleProvider {
-        return object : FrameworkSupportInModuleProvider() {
+    override fun createProvider() =
+        object : FrameworkSupportInModuleProvider() {
             override fun getFrameworkType() = this@KUtilsFramework
 
-            override fun createConfigurable(model: FrameworkSupportModel): FrameworkSupportInModuleConfigurable {
-                return object : FrameworkSupportInModuleConfigurable() {
-                    override fun createComponent(): JComponent? {
-                        return JCheckBox("SDK Extra Option")
-                    }
+            override fun createConfigurable(model: FrameworkSupportModel) =
+                object : FrameworkSupportInModuleConfigurable() {
+                    override fun createComponent(): JComponent? = null
 
                     override fun addSupport(module: Module, model: ModifiableRootModel, provider: ModifiableModelsProvider) {
-                        // This is the place to set up a library, generate a specific file, etc
-                        // and actually add framework support to a module.
+                        //TODO Add KUtils support
                     }
                 }
-            }
 
             override fun isEnabledForModuleType(type: ModuleType<*>) = true
         }
-    }
 
     override fun getPresentableName() = "KUtils Framework"
-
     override fun getIcon() = ICONS.KUTILS_16
-
-    companion object {
-        const val FRAMEWORK_ID = "me.jakejmattson.kutils.framework.KUtilsFramework"
-    }
 }
